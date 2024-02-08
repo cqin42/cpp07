f689df2fb6f05ea0e57e3c728b05ac820a1f1107
Christine Qin <christine236315@gmail.com>
2024-02-09 00:06:18 +0100

    ex02

--- ex02/Array.cpp
+++ ex02/Array.cpp
@@ -6,16 +6,67 @@
 /*   By: christine <christine@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 09:48:15 by christine         #+#    #+#             */
-/*   Updated: 2024/02/06 11:05:24 by christine        ###   ########.fr       */
+/*   Updated: 2024/02/09 00:04:10 by christine        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
 
-Array::Array()
+template<typename T>
+Array<T>::Array():_data(NULL), _size(0)
 {
 }
-Array::Array(unsigned int n);
-Array::Array(const Array &autre);
-Array &Array::operator=(const &autre);
-Array::~Array();
+template<typename T>
+Array<T>::Array(unsigned int n): _size(n)
+{
+	_data = new T[n];
+}
+template<typename T>
+Array<T>::Array(const Array &autre): _size(autre._size)
+{
+	_data = new T[autre._size];
+	for (unsigned int i = 0; i < autre._size; i++)
+	{
+		_data[i] = autre._data[i];
+	}
+}
+
+template<typename T>
+Array<T> &Array<T>::operator=(const Array &autre)
+{
+	if (this != &autre)
+	{
+		if (_data)
+			delete[] _data;
+		_size = autre._size;
+		_data = new T[autre._size];
+		for (unsigned int i = 0; i < autre._size; i++)
+		{
+			_data[i] = autre._data[i];
+		}
+	}
+	return (*this);
+}
+
+template<typename T>
+Array<T>::~Array()
+{
+	if (_data)
+		delete[] _data;
+}
+
+template<typename T>
+T &Array<T>::operator[](unsigned int i) const
+{
+	if (i >= _size)
+		throw OutofBoundsException();
+	return (_date[i]);
+}
+
+
+template<typename T>
+unsigned int Array<T>::size() const
+{
+	return (_size);
+}
+
--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: christine <christine@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/06 09:39:29 by christine         #+#    #+#             */
-/*   Updated: 2024/02/06 11:00:52 by christine        ###   ########.fr       */
+/*   Updated: 2024/02/09 00:03:49 by christine        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,30 @@
 
 #include <iostream>
 
-template<typedef T>
-
+template<typename T>
 class Array
 {
 	private:
-
+		unsigned int _size;
+		T *_array;
 	public:
 		Array();
 		Array(unsigned int n);
 		Array(const Array &autre);
-		Array &operator=(const &autre);
+		Array &operator=(const Array &autre);
 		~Array();
+
+		T &operator[](unsigned int i) const;
+		unsigned int size() const;
+
+		class OutofBoundsException: public std::exception
+		{
+			public:
+				virtual const char* what() const throw
+				{
+					return ("index is out of bounds");
+				}
+		}
 };
 
 
